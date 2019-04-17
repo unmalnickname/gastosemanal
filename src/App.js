fcdd07be740b9bcd4e138976c8f579916faafe6b
Luis Avila <x4288177@gmail.com>
2019-04-17 10:16:57 -0400

    :tada: Initialzin the App Components Structure.
    The header and CSS files has been organize #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./css/App.css";
+import Header from "./componentes/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App wool container">
+        <Header titulo="Gasto Semanal" />
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column">1</div>
+            <div className="one-half column">2</div>
+          </div>
+        </div>
       </div>
     );
   }
