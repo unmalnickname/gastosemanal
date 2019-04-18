8358a5f4b48ec1538e960d4c7e7a009d6b5d1b91
Luis Avila <x4288177@gmail.com>
2019-04-18 09:09:38 -0400

    :lipstick: 4. Creando el Formulario para crear gastos
     #4

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-import "./css/App.css";
-import Header from "./componentes/Header";
+import "../css/App.css";
+import Header from "./Header";
+import Formulario from "./Formulario";
 
 class App extends Component {
   render() {
     return (
-      <div className="App wool container">
+      <div className="App container">
         <Header titulo="Gasto Semanal" />
         <div className="contenido-principal contenido">
           <div className="row">
-            <div className="one-half column">1</div>
+            <div className="one-half column">
+              {" "}
+              <Formulario />
+            </div>
             <div className="one-half column">2</div>
           </div>
         </div>
